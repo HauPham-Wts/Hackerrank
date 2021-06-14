5931bace6239d7740769307c309b585271de392a
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 09:08:22 +0700

    Create Funix-Lab101-Asm1-Java Stdin & Stdout.java

---
+++ Funix-Lab101-Asm1-Java Stdin & Stdout.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        System.out.print(a + "\n" + b + "\n" + c);
+    }
+}
