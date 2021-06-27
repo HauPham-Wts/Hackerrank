daba90d38534e9c0611845f2b43826b9ac1a359b
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-27 22:50:58 +0700

    Create Funix-Lab101-Asm4-Java Factory Pattern.java

---
+++ Funix-Lab101-Asm4-Java Factory Pattern.java
@@ -0,0 +1,7 @@
+        if ("pizza".equals(order)) {
+            return new Pizza();
+        } else if ("cake".equals(order)) {
+            return new Cake();
+        } else {
+            return null;
+        }
